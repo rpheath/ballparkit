3420d520afde5e2e487d334f230ca6015bbd09a1
rpheath <rpheath@gmail.com>
2009-01-21 11:50:31 -0500

    fade flash messags

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,7 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+  setTimeout(hideFlashes, 25000);
+});
+
+var hideFlashes = function() {
+  $('p.notice, p.warning, p.error').fadeOut(1500);
+}
