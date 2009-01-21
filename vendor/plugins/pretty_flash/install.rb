57dc48d68fe278571837c8f824567da7e2a739c0
rpheath <rpheath@gmail.com>
2009-01-21 11:51:52 -0500

    added pretty_flash plugin

---
+++ vendor/plugins/pretty_flash/install.rb
@@ -0,0 +1,23 @@
+message = <<-MESSAGE
+  
+  ********************************************
+  To get started, navigate to your project
+  root, and type:
+  
+  $ rake pretty_flash:install
+  
+  That will copy all of the necessary assets
+  to their appropriate homes.
+  
+  !! WARNING: there is no protection against
+     file collision, so your files will be
+     overwritten if they already exist. You
+     may want to check that first! (see the
+     pretty_flash/assets/ to check)
+  
+  Enjoy!
+  ********************************************
+  
+MESSAGE
+
+puts message
