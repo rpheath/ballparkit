f9934799a6fe8210426b941267840966482f5903
rpheath <rpheath@gmail.com>
2009-01-22 12:12:40 -0500

    added navigation and cleaned up headeer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'estimates'
   
   map.resource :session
+  map.resources :estimates
+  map.resources :settings
   
   map.with_options :controller => 'sessions' do |path|
     path.login '/login', :action => 'new'
