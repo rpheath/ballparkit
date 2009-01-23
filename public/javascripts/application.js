6e05f7e9388cb10ebafef2a8afca16f939a3f4a7
rpheath <rpheath@gmail.com>
2009-01-23 15:41:27 -0500

    working on add/delete inline tasks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,30 @@ $(document).ready(function() {
   setTimeout(hideFlashes, 25000);
   
   $(':input:visible:enabled:first').focus()
+  
+  function reloadZebra() {
+    $('table.tasks tr').removeClass('odd');
+    $('table.tasks tr:odd').addClass('odd');
+  }
+  
+  $('table.tasks a.delete').click(function() {
+    $(this).parent().parent().remove();
+    reloadZebra();
+    return false;
+  });
+  
+  $('a.new').click(function() {
+    var table = $('table.tasks'),
+        row = $('table.tasks tr:last').clone(true);
+    
+    $(row).find('td input.text').each(function(i) {
+      $(this).val('');
+      $(this).attr('name', $(this).attr('name').replace(/\d+/, ''));
+      $(this).attr('id', $(this).attr('id').replace(/\d+/, ''));
+    });
+    
+    row.insertAfter('table.tasks tr:last');
+    reloadZebra();
+    return false;
+  });
 });
