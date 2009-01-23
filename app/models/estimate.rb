e935fefa743203be18a0dcab07bf6a955f7790dc
rpheath <rpheath@gmail.com>
2009-01-23 11:41:53 -0500

    added total estimate

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -3,4 +3,10 @@ class Estimate < ActiveRecord::Base
   associated_save :tasks
   
   validates_presence_of :title
+  
+  def total
+    tasks.inject(0) do |sum, task|
+      sum += task.rate.to_i * task.hours.to_i
+    end.to_s
+  end
 end
