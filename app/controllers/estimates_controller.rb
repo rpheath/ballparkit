3a277bf780c955e96fd82e1fa175ccae614ddd65
rpheath <rpheath@gmail.com>
2009-01-23 11:42:08 -0500

    now showing current_user estimates

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,6 +1,6 @@
 class EstimatesController < ApplicationController
   def index
-    render
+    @estimates = current_user.estimates
   end
   
   def new
@@ -16,5 +16,26 @@ class EstimatesController < ApplicationController
   rescue ActiveRecord::RecordInvalid
     @estimate.tasks.build params[:estimate][:_tasks]
     render :action => 'new'
+  end
+  
+  def edit
+    @estimate = Estimate.find(params[:id])
+  end
+  
+  def update
+    @estimate = Estimate.find(params[:id])
+    @estimate.update_attributes!(params[:estimate])
+    notice "Successfully updated #{@estimate.title}"
+    redirect_to estimates_path
+  rescue ActiveRecord::RecordInvalid
+    render :action => 'edit'
+  end
+  
+  def destroy
+    @estimate = Estimate.find(params[:id])
+    title = @estimate.title
+    @estimate.destroy
+    notice "Successfully deleted your #{title} estimate"
+    redirect_to estimates_path
   end
 end
