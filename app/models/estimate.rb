c149a07d9a5beca1cb1a8940d4537176b2aa9317
rpheath <rpheath@gmail.com>
2009-01-24 01:12:54 -0500

    forgot that it belonged to a user

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,13 +1,15 @@
 class Estimate < ActiveRecord::Base
   has_many :tasks, :attributes => true, :dependent => :nullify
+  belongs_to :user
   
   named_scope :descending, :order => 'id DESC'
   
   validates_presence_of :title
   
   def self.paginated(user, page)
-    descending.paginate(:all, :conditions => { :user_id => user.id }, 
-      :page => page, :per_page => 10)
+    descending.paginate(:all, :conditions => { 
+        :user_id => user.id 
+      }, :page => page, :per_page => 10)
   end
   
   def total
