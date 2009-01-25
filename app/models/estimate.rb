fdb6008bd39d3ad84bbfbbdacd2904101a61912b
rpheath <rpheath@gmail.com>
2009-01-25 09:21:19 -0500

    added per_page option to paginated estimates

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -6,10 +6,10 @@ class Estimate < ActiveRecord::Base
   
   validates_presence_of :title
   
-  def self.paginated(user, page)
+  def self.paginated(user, page, options = {})
     descending.paginate(:all, :conditions => { 
         :user_id => user.id 
-      }, :page => page, :per_page => 10)
+      }, :page => page, :per_page => options[:per_page] || 10)
   end
   
   def total
