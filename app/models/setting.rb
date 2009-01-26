7a0c4bf9218aafc453c36604f4f2fa903e972c01
rpheath <rpheath@gmail.com>
2009-01-25 23:11:45 -0500

    added partial layout for settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,15 @@
+require 'ostruct'
+
 class Setting < ActiveRecord::Base
   belongs_to :user
   has_many :default_tasks, :attributes => true,
     :discard_if => proc { |task| task.description.blank? }
+  
+  def self.support
+    OpenStruct.new(
+      :default_rate => 'Default Rate support',
+      :use_default_rate => 'Using Default Rate support',
+      :tasks => 'Tasks support'
+    )
+  end
 end
