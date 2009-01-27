ac67a084050415cc4c8f5684298798f877dfba0c
rpheath <rpheath@gmail.com>
2009-01-27 16:17:11 -0500

    just formatting

--- app/helpers/estimates_helper.rb
+++ app/helpers/estimates_helper.rb
@@ -1,6 +1,6 @@
 module EstimatesHelper
   def default_rate
     return unless @estimate.new_record? && current_user.setting.use_default_rate?
-    current_user.setting.default_rate
+    number_to_currency(current_user.setting.default_rate).gsub('$', '')
   end
 end
