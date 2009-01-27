fc83dd9b6926b6e63fd379660d8f968cdd15930f
rpheath <rpheath@gmail.com>
2009-01-27 14:52:46 -0500

    estimate form now totals automatically as you type

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,6 +7,41 @@ $.fn.zebra = function() {
     end().find('tr:odd').addClass('odd')
 }
 
+$.fn.toCurrency = function() {
+  $(this).text('$' + Math.abs($(this).text()).toFixed(2))
+}
+
+$.fn.writeTaskTotal = function(hours, rate) {
+  $(this).html(hours * rate).toCurrency()
+}
+
+$.extend({
+  estimate: {
+    total: function(options) {
+      var options = $.extend({
+        reload: false
+      }, options), total = 0
+      
+      if (options.reload) $.estimate.totalTasks()
+      
+      $('.tasks .estimate span').each(function() {
+        total += Math.abs($(this).text().replace(/\$|\,/g, ''))
+      })
+
+      $('#total span').text(total).toCurrency()
+    },
+    totalTasks: function() {
+      $('input.hours, input.rate').each(function() {
+        var hours = $(this).parents('.task').find('input.hours').val(),
+            rate  = $(this).parents('.task').find('input.rate').val().replace('$', '')
+            span  = $(this).parents('.task').find('.estimate span')
+
+        span.writeTaskTotal(hours, rate)
+      })
+    }
+  }
+})
+
 $(document).ready(function() {
   setTimeout(hideFlashes, 25000)
   
@@ -18,6 +53,16 @@ $(document).ready(function() {
     $(this).parents('form')[0].reset()
     return false
   })
+  
+  $('form a.rate').click(function() {
+    $('input.rate').val($(this).text().replace('$', ''))
+    $.estimate.total({reload: true})
+    return false
+  })
+  
+  $.estimate.totalTasks()
+  $.estimate.total()
+  $('input.hours, input.rate').bind('keyup', function() { $.estimate.total({reload: true}) })
   
   $('ul.estimates li').mouseover(function() {
     $(this).addClass('highlight').find('span.options').show()
