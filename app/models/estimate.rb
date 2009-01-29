01b0ff63aa896ce5d71d1ffb90f3117b2105ddac
rpheath <rpheath@gmail.com>
2009-01-29 11:39:25 -0500

    added unit tests for Estimate model

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -16,7 +16,7 @@ class Estimate < ActiveRecord::Base
   def self.paginated(user, page, options = {})
     descending.paginate(:all, :conditions => { 
         :user_id => user.id 
-      }, :page => page, :per_page => options[:per_page] || 10)
+      }, :page => page, :per_page => options[:per_page] || per_page || 10)
   end
   
   def total(type)
--- test/unit/estimate_test.rb
+++ test/unit/estimate_test.rb
@@ -1,8 +1,75 @@
 require 'test_helper'
 
 class EstimateTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  private
+    def setup
+      @estimate = Estimate.new(:title => 'ACME Inc.')
+      Estimate.delete_all
+    end
+    
+    def create_estimates(count = 5)
+      count.times { |n| Estimate.create(:user_id => users(:ryan).id, :title => "Estimate #{n}") }
+    end
+    
+    def tasks(estimate_id)
+      [
+        Task.new(:estimate_id => estimate_id, :description => 'Task 1', :hours => '1', :rate => '$10'),
+        Task.new(:estimate_id => estimate_id, :description => 'Task 2', :hours => '2', :rate => '$10'),
+        Task.new(:estimate_id => estimate_id, :description => 'Task 3', :hours => '3', :rate => '$10')
+      ]
+    end
+    
+  public
+    test "should be a valid estimate" do
+      assert_valid @estimate
+    end
+    
+    test "should require an estimate title" do
+      assert_no_difference 'Estimate.count' do
+        e = Estimate.create
+        assert e.errors.on(:title)
+      end
+    end
+    
+    test "should list estimates in descending order by id" do
+      create_estimates
+      
+      estimate_ids = Estimate.paginated(users(:ryan), 1).map(&:id)
+      assert_equal estimate_ids.first, estimate_ids.max
+    end
+    
+    test "should support pagination" do
+      create_estimates
+      Estimate.stubs(:per_page).returns(2)
+      
+      estimates = Estimate.paginated(users(:ryan), 1)
+      assert_equal 2, estimates.size.to_i
+    end
+    
+    test "should create a unique SHA1 token after create" do
+      assert_nil @estimate.token
+      
+      @estimate.save
+      
+      assert_not_nil @estimate.token
+      assert_match /^[a-zA-Z0-9]{40}$/, @estimate.token
+    end
+    
+    test "should total hours" do
+      @estimate.save
+      @estimate.expects(:tasks).returns(tasks(@estimate.id))
+      
+      assert_equal '6.0', @estimate.total(:hours)
+    end
+    
+    test "should total price" do
+      @estimate.save
+      @estimate.expects(:tasks).returns(tasks(@estimate.id))
+      
+      assert_equal '60.0', @estimate.total(:price)
+    end
+    
+    test "should return 0 for totals other than hours or price" do
+      assert_equal '0', @estimate.total(:other)
+    end
 end
