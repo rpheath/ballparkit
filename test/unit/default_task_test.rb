a2db8e700d8bc4e6f134521c2b16f56c20e48224
rpheath <rpheath@gmail.com>
2009-01-29 11:50:23 -0500

    added unit tests for DefaultTask model

--- test/unit/default_task_test.rb
+++ test/unit/default_task_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class DefaultTaskTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  private
+    def setup
+      @setting = Setting.create
+      
+      DefaultTask.create(:setting_id => @setting.id, :description => 'Information Architecture')
+      DefaultTask.create(:setting_id => @setting.id, :description => 'XHTML/CSS Development')
+      DefaultTask.create(:setting_id => @setting.id, :description => 'Server-side Development')
+    end
+  
+  public
+    test "should return true/false for task existence by setting and description (true case)" do
+      assert DefaultTask.found?(@setting.id, 'Information Architecture')
+    end
+    
+    test "should return true/false for task existence by setting and description (false case)" do
+      assert !DefaultTask.found?(@setting.id, 'Testing & Debugging')
+    end
+    
+    test "should not care if description of task is proper case" do
+      assert DefaultTask.found?(@setting.id, 'information architecture')
+    end
 end
