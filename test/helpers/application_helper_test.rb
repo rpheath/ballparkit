077a9248255f7f921e715897919ea023a8d65543
rpheath <rpheath@gmail.com>
2009-01-29 13:41:09 -0500

    added helper tests

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,7 +1,25 @@
 require File.join(File.dirname(__FILE__), '/../test_helper')
 
 class ApplicationHelperTest < ActionView::TestCase
-  test "something" do
-    assert true
+  test "should set page title without a block" do
+    response = page_title('Heading')
+    
+    assert_equal '<h1>Heading</h1>', response
+  end
+  
+  test "should set page title with a block" do
+    response = page_title do
+      '<em>Heading</em>'
+    end
+    
+    assert_equal '<h1><em>Heading</em></h1>', response
+  end
+  
+  test "should section content with a header and yield content" do
+    response = section('User Registration') do
+      '<input type="text" name="fullname" />'
+    end
+
+    assert_equal '<input type="text" name="fullname" />', response
   end
 end
--- test/helpers/estimates_helper_test.rb
+++ test/helpers/estimates_helper_test.rb
@@ -1,7 +1,40 @@
 require File.join(File.dirname(__FILE__), '/../test_helper')
 
 class EstimatesHelperTest < ActionView::TestCase
-  test "something" do
-    assert true
-  end
+  private
+    def setup
+      @estimate, @setting = Estimate.new, Setting.new
+      current_user.stubs(:setting).returns(@setting)
+      @setting.stubs(:default_rate).returns('75.00')
+    
+      @estimate.stubs(:new_record?).returns(true)
+      @setting.stubs(:use_default_rate?).returns(true)
+    end
+  
+    def current_user
+      users(:ryan)
+    end
+
+  public
+    test "should return default rate for new records" do
+      assert_equal '75.00', default_rate
+    end
+  
+    test "should return default rate for any format of price" do
+      @setting.stubs(:default_rate).returns('80')
+    
+      assert_equal '80.00', default_rate
+    end
+  
+    test "should return nil for existing records" do
+      @estimate.stubs(:new_record?).returns(false)
+    
+      assert_nil default_rate
+    end
+  
+    test "should return nil if user settings say not to use default rate" do
+      @setting.stubs(:use_default_rate?).returns(false)
+    
+      assert_nil default_rate
+    end
 end
