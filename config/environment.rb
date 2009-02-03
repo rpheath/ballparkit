5223f42de9028be5f125cadbda235e0ee71a86a3
rpheath <rpheath@gmail.com>
2009-02-03 14:25:29 -0500

    updating configuration for production

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-# ENV['RAILS_ENV'] ||= 'production'
+#ENV['RAILS_ENV'] ||= 'production'
 
 RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
@@ -20,14 +20,19 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+	  File.directory?(lib = "#{dir}/lib") ? lib : dir
+	end
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
-  config.time_zone = 'UTC'
+  config.time_zone = 'Eastern Time (US & Canada)'
 
   config.action_controller.session = {
     :session_key => '_ballpark_session',
     :secret      => '613c7b031ce2968fa5b09947b8c3ff12e8a4e35ba123e622efc13f1f5cd5dd1c2006bb6f8712c36fc1da13b7c7e95ee11148be0ebed076327d13b6bc236236e9'
   }
 end
+
+%w(will_paginate openid).each { |lib| require lib }
