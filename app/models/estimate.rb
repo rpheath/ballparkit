b65d539844ac28f1874ed1cc002d63d4621b8e49
rpheath <rpheath@gmail.com>
2009-02-04 14:39:12 -0500

    only discard if desc, hours, and rate are blank

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -22,7 +22,7 @@ class Estimate < ActiveRecord::Base
   def self.paginated(user, page, options = {})
     descending.paginate(:all, :conditions => { 
         :user_id => user.id 
-      }, :page => page, :per_page => options[:per_page] || per_page || 10)
+      }, :page => page, :per_page => options[:per_page] || per_page)
   end
   
   def self.find_by_token!(token)
