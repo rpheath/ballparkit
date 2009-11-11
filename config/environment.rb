3eb43fa8a3823e7690edc2bd0fee3129ae5fd23d
rpheath <rpheath@gmail.com>
2009-11-10 21:37:12 -0500

    stay consistent with frozen rails until upgraded

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
